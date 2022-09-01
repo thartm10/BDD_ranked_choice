cc9546432e9b1d8589325a0a27eec6d115709b8c
thartm10 <70657022+thartm10@users.noreply.github.com>
2022-09-01 11:35:18 -0400

    Add files via upload

--- BDDcsvMaker.py
+++ BDDcsvMaker.py
@@ -4,14 +4,19 @@ import random
 doctors, patients = int(sys.argv[1]), int(sys.argv[2])
 outputfile = open("patient_preference_list.csv",'w')
 doctorPatientMap = {}
+notFirst = False
 
 for i in range(patients):
+    if notFirst:outputfile.write(",")
     doctorPatientMap[i] = random.sample(list(range(doctors)),doctors)
-    outputfile.write(","+str(i))
+    outputfile.write(str(i))
+    notFirst = True
 outputfile.write("\n")
 
 for j in range(doctors):
-    outputfile.write(str(j))
+    notFirst2 = False
     for k in range(patients):
-        outputfile.write(","+str(doctorPatientMap[k].index(j)))
+        if notFirst2:outputfile.write(",")
+        outputfile.write(str(doctorPatientMap[k].index(j)))
+        notFirst2 = True
     outputfile.write("\n")
